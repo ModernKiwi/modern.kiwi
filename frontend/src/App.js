50eefadb4cdd1b6645238d7c546a56e35f04b213
Alvin Fletcher <alvin@modern.kiwi>
2022-02-22 15:56:01 +1300

    Added React framework, setup Hello World

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
